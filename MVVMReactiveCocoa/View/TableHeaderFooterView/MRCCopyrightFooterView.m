6397500f223b7cad3d791b96a728cf18d352131a
leichunfeng <307213080@qq.com>
2015-03-06 01:31:15 +0800

    Add Copyright in About UI.
    Former-commit-id: 7f9bbdede30aa18ed8017fd150cd75e0c7f20e61

---
+++ MVVMReactiveCocoa/View/TableHeaderFooterView/MRCCopyrightFooterView.m
@@ -0,0 +1,21 @@
+//
+//  MRCCopyrightFooterView.m
+//  MVVMReactiveCocoa
+//
+//  Created by leichunfeng on 15/3/6.
+//  Copyright (c) 2015年 leichunfeng. All rights reserved.
+//
+
+#import "MRCCopyrightFooterView.h"
+
+@implementation MRCCopyrightFooterView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
