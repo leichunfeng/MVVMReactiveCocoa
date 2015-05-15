111f81bd7621cabb7701883e4f56417ffa115330
leichunfeng <307213080@qq.com>
2015-05-15 23:47:07 +0800

    Perfect repo settings.
    Former-commit-id: f298d35336ebe22298972d1db44565e4a6fca8cf

---
+++ MVVMReactiveCocoa/View/TableViewCell/MRCRepoSettingsOwnerTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  MRCRepoSettingsOwnerTableViewCell.h
+//  MVVMReactiveCocoa
+//
+//  Created by leichunfeng on 15/5/15.
+//  Copyright (c) 2015年 leichunfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRCRepoSettingsOwnerTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *avatarImageView;
+@property (weak, nonatomic) IBOutlet UILabel *topTextLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomTextLabel;
+
+@end
---
+++ MVVMReactiveCocoa/View/TableViewCell/MRCRepoSettingsOwnerTableViewCell.m
@@ -0,0 +1,18 @@
+//
+//  MRCRepoSettingsOwnerTableViewCell.m
+//  MVVMReactiveCocoa
+//
+//  Created by leichunfeng on 15/5/15.
+//  Copyright (c) 2015年 leichunfeng. All rights reserved.
+//
+
+#import "MRCRepoSettingsOwnerTableViewCell.h"
+
+@implementation MRCRepoSettingsOwnerTableViewCell
+
+- (void)awakeFromNib {
+    self.avatarImageView.layer.cornerRadius = 5;
+    self.avatarImageView.clipsToBounds = YES;
+}
+
+@end
