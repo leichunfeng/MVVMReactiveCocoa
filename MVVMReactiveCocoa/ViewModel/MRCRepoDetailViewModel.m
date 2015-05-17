9707b24e09e612452cfe9448c640397fb7bc103b
leichunfeng <307213080@qq.com>
2015-05-17 22:10:00 +0800

    Fix bug.

--- MVVMReactiveCocoa/ViewModel/MRCRepoDetailViewModel.m
+++ MVVMReactiveCocoa/ViewModel/MRCRepoDetailViewModel.m
@@ -136,9 +136,10 @@
             RACTupleUnpack(OCTRepository *repo, NSString *readmeHTMLString) = tuple;
             
             dispatch_async(dispatch_get_main_queue(), ^{
+                repo.isStarred = self.repository.isStarred;
                 [self.repository mergeValuesForKeysFromModel:repo];
                 dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                    [self.repository mrc_saveOrUpdate];
+                    [self.repository mrc_update];
                 });
             });
             
