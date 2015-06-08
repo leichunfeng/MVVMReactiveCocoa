0a828a1537e6ccc89cfc064c786eb5d63032ae16
leichunfeng <307213080@qq.com>
2015-06-08 22:37:02 +0800

    Adjust code style.

--- MVVMReactiveCocoa/Model/MRCAppStoreServiceImpl.m
+++ MVVMReactiveCocoa/Model/MRCAppStoreServiceImpl.m
@@ -11,26 +11,29 @@
 @implementation MRCAppStoreServiceImpl
 
 - (RACSignal *)requestAppInfoFromAppStoreWithAppID:(NSString *)appID {
-    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
-        MKNetworkEngine *networkEngine = [[MKNetworkEngine alloc] initWithHostName:@"itunes.apple.com"];
-        
-        MKNetworkOperation *operation = [networkEngine operationWithPath:@"lookup"
-                                                                  params:@{ @"id": appID }
-                                                              httpMethod:@"GET"];
-        
-        [operation addCompletionHandler:^(MKNetworkOperation *completedOperation) {
-            [subscriber sendNext:completedOperation.responseJSON];
-            [subscriber sendCompleted];
-        } errorHandler:^(MKNetworkOperation *completedOperation, NSError *error) {
-            [subscriber sendError:error];
-        }];
-        
-        [networkEngine enqueueOperation:operation];
-        
-        return [RACDisposable disposableWithBlock:^{
-            [operation cancel];
-        }];
-    }];
+    return [[[RACSignal
+        createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+            MKNetworkEngine *networkEngine = [[MKNetworkEngine alloc] initWithHostName:@"itunes.apple.com"];
+            
+            MKNetworkOperation *operation = [networkEngine operationWithPath:@"lookup"
+                                                                      params:@{ @"id": appID }
+                                                                  httpMethod:@"GET"];
+            
+            [operation addCompletionHandler:^(MKNetworkOperation *completedOperation) {
+                [subscriber sendNext:completedOperation.responseJSON];
+                [subscriber sendCompleted];
+            } errorHandler:^(MKNetworkOperation *completedOperation, NSError *error) {
+                [subscriber sendError:error];
+            }];
+            
+            [networkEngine enqueueOperation:operation];
+            
+            return [RACDisposable disposableWithBlock:^{
+                [operation cancel];
+            }];
+        }]
+        replayLazily]
+        setNameWithFormat:@"-requestAppInfoFromAppStoreWithAppID: %@", appID];
 }
 
 @end
--- MVVMReactiveCocoa/Model/MRCRepositoryServiceImpl.m
+++ MVVMReactiveCocoa/Model/MRCRepositoryServiceImpl.m
@@ -11,36 +11,39 @@
 @implementation MRCRepositoryServiceImpl
 
 - (RACSignal *)requestRepositoryReadmeHTMLString:(OCTRepository *)repository reference:(NSString *)reference {
-    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
-        NSString *accessToken   = [SSKeychain accessToken];
-        NSString *authorization = [NSString stringWithFormat:@"token %@", accessToken];
-        
-        MKNetworkEngine *networkEngine = [[MKNetworkEngine alloc] initWithHostName:@"api.github.com"
-                                                                customHeaderFields:@{ @"Authorization": authorization}];
-        
-        NSString *path = [NSString stringWithFormat:@"repos/%@/%@/readme", repository.ownerLogin, repository.name];
-        MKNetworkOperation *operation = [networkEngine operationWithPath:path
-                                                                  params:@{@"ref": reference}
-                                                              httpMethod:@"GET"
-                                                                     ssl:YES];
-        [operation addHeaders:@{ @"Accept": @"application/vnd.github.VERSION.html" }];
-        [operation addCompletionHandler:^(MKNetworkOperation *completedOperation) {
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                [subscriber sendNext:completedOperation.responseString];
-                [subscriber sendCompleted];
-            });
-        } errorHandler:^(MKNetworkOperation *completedOperation, NSError *error) {
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                [subscriber sendError:error];
-            });
-        }];
-        
-        [networkEngine enqueueOperation:operation];
-        
-        return [RACDisposable disposableWithBlock:^{
-            [operation cancel];
-        }];
-    }];
+    return [[[RACSignal
+        createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+            NSString *accessToken   = [SSKeychain accessToken];
+            NSString *authorization = [NSString stringWithFormat:@"token %@", accessToken];
+            
+            MKNetworkEngine *networkEngine = [[MKNetworkEngine alloc] initWithHostName:@"api.github.com"
+                                                                    customHeaderFields:@{ @"Authorization": authorization}];
+            
+            NSString *path = [NSString stringWithFormat:@"repos/%@/%@/readme", repository.ownerLogin, repository.name];
+            MKNetworkOperation *operation = [networkEngine operationWithPath:path
+                                                                      params:@{@"ref": reference}
+                                                                  httpMethod:@"GET"
+                                                                         ssl:YES];
+            [operation addHeaders:@{ @"Accept": @"application/vnd.github.VERSION.html" }];
+            [operation addCompletionHandler:^(MKNetworkOperation *completedOperation) {
+                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                    [subscriber sendNext:completedOperation.responseString];
+                    [subscriber sendCompleted];
+                });
+            } errorHandler:^(MKNetworkOperation *completedOperation, NSError *error) {
+                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                    [subscriber sendError:error];
+                });
+            }];
+            
+            [networkEngine enqueueOperation:operation];
+            
+            return [RACDisposable disposableWithBlock:^{
+                [operation cancel];
+            }];
+        }]
+        replayLazily]
+        setNameWithFormat:@"-requestRepositoryReadmeHTMLString: %@ reference: %@", repository, reference];
 }
 
 @end
