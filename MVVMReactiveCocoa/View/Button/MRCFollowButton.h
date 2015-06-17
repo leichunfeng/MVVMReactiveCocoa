cd0924de6b7126988046aa5607e0cd5a300f950e
leichunfeng <307213080@qq.com>
2015-06-18 00:17:13 +0800

    Abstract follow button.

---
+++ MVVMReactiveCocoa/View/Button/MRCFollowButton.h
@@ -0,0 +1,13 @@
+//
+//  MRCFollowButton.h
+//  MVVMReactiveCocoa
+//
+//  Created by leichunfeng on 15/6/17.
+//  Copyright (c) 2015年 leichunfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRCFollowButton : UIButton
+
+@end
