9f0d021ab1bab9e7252eaa5b9ff49e5347a81df7
leichunfeng <307213080@qq.com>
2015-06-19 16:06:04 +0800

    Refactoring all repo list.
    Former-commit-id: 2ec17c8e01df89231153731126757450450a5056

--- MVVMReactiveCocoa/ViewModel/MRCPublicReposViewModel.m
+++ MVVMReactiveCocoa/ViewModel/MRCPublicReposViewModel.m
@@ -14,8 +14,42 @@
     return MRCReposViewModelTypePublic;
 }
 
+- (MRCReposViewModelOptions)options {
+    MRCReposViewModelOptions options = 0;
+    
+    if (self.isCurrentUser) {
+        options = options | MRCReposViewModelOptionsFetchLocalDataOnInitialize;
+//        options = options | MRCReposViewModelOptionsObserveStarredReposChange;
+        options = options | MRCReposViewModelOptionsSaveOrUpdateRepos;
+//        options = options | MRCReposViewModelOptionsSaveOrUpdateStarredStatus;
+        options = options | MRCReposViewModelOptionsPagination;
+//        options = options | MRCReposViewModelOptionsSectionIndex;
+    } else {
+//        options = options | MRCReposViewModelOptionsFetchLocalDataOnInitialize;
+//        options = options | MRCReposViewModelOptionsObserveStarredReposChange;
+//        options = options | MRCReposViewModelOptionsSaveOrUpdateRepos;
+//        options = options | MRCReposViewModelOptionsSaveOrUpdateStarredStatus;
+        options = options | MRCReposViewModelOptionsPagination;
+//        options = options | MRCReposViewModelOptionsSectionIndex;
+    }
+    
+    return options;
+}
+
 - (NSArray *)fetchLocalData {
     return [OCTRepository mrc_fetchUserPublicRepositoriesWithPage:1 perPage:self.perPage];
+}
+
+- (RACSignal *)requestRemoteDataSignalWithPage:(NSUInteger)page {
+    return [[[self.services
+    	client]
+    	fetchRepositoriesWithUser:self.user page:page perPage:self.perPage].collect
+    	map:^(NSArray *repositories) {
+            if (page != 1) {
+                repositories = @[ (self.repositories ?: @[]).rac_sequence, repositories.rac_sequence ].rac_sequence.flatten.array;
+            }
+            return repositories;
+        }];
 }
 
 @end
--- MVVMReactiveCocoa/ViewModel/MRCStarredReposViewModel.m
+++ MVVMReactiveCocoa/ViewModel/MRCStarredReposViewModel.m
@@ -14,39 +14,52 @@
     return MRCReposViewModelTypeStarred;
 }
 
+- (MRCReposViewModelOptions)options {
+    MRCReposViewModelOptions options = 0;
+    
+    if (self.isCurrentUser) {
+        options = options | MRCReposViewModelOptionsFetchLocalDataOnInitialize;
+        options = options | MRCReposViewModelOptionsObserveStarredReposChange;
+        options = options | MRCReposViewModelOptionsSaveOrUpdateRepos;
+        options = options | MRCReposViewModelOptionsSaveOrUpdateStarredStatus;
+//        options = options | MRCReposViewModelOptionsPagination;
+        options = options | MRCReposViewModelOptionsSectionIndex;
+    } else {
+//        options = options | MRCReposViewModelOptionsFetchLocalDataOnInitialize;
+//        options = options | MRCReposViewModelOptionsObserveStarredReposChange;
+//        options = options | MRCReposViewModelOptionsSaveOrUpdateRepos;
+//        options = options | MRCReposViewModelOptionsSaveOrUpdateStarredStatus;
+        options = options | MRCReposViewModelOptionsPagination;
+//        options = options | MRCReposViewModelOptionsSectionIndex;
+    }
+    
+    return options;
+}
+
 - (NSArray *)fetchLocalData {
     return [OCTRepository mrc_fetchUserStarredRepositories];
 }
 
 - (RACSignal *)requestRemoteDataSignalWithPage:(NSUInteger)page {
     if (self.isCurrentUser) {
-        return [[[[self.services
+        return [[[self.services
         	client]
-        	fetchUserStarredRepositories]
-            collect]
-            doNext:^(NSArray *repositories) {
+        	fetchUserStarredRepositories].collect
+            map:^(NSArray *repositories) {
                 for (OCTRepository *repo in repositories) {
                     repo.starredStatus = OCTRepositoryStarredStatusYES;
                 }
-                self.repositories = [repositories sortedArrayUsingComparator:^NSComparisonResult(OCTRepository *repo1, OCTRepository *repo2) {
-                    return [repo1.name caseInsensitiveCompare:repo2.name];
-                }];
-                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                    [OCTRepository mrc_saveOrUpdateRepositories:repositories];
-                    [OCTRepository mrc_saveOrUpdateStarredStatusWithRepositories:repositories];
-                });
+                return repositories;
             }];
     } else {
-        return [[[[self.services
+        return [[[self.services
         	client]
-        	fetchStarredRepositoriesWithUser:self.user page:page perPage:self.perPage]
-        	collect]
-        	doNext:^(NSArray *repositories) {
-                if (page == 1) {
-                    self.repositories = repositories;
-                } else {
-                    self.repositories = @[ (self.repositories ?: @[]).rac_sequence, repositories.rac_sequence ].rac_sequence.flatten.array;
+        	fetchStarredRepositoriesWithUser:self.user page:page perPage:self.perPage].collect
+        	map:^(NSArray *repositories) {
+                if (page != 1) {
+                    repositories = @[ (self.repositories ?: @[]).rac_sequence, repositories.rac_sequence ].rac_sequence.flatten.array;
                 }
+                return repositories;
             }];
     }
 }
