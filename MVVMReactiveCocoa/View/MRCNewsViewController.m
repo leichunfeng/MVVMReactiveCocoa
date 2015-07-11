62e922784a52ca6aaf1076873552cf02e3eb8eae
leichunfeng <307213080@qq.com>
2015-07-11 14:42:26 +0800

    Perfect news list.
    Former-commit-id: 04fc69a8a5d8375f61a4f4e4b608b9760576d2e9

--- MVVMReactiveCocoa/View/MRCNewsViewController.m
+++ MVVMReactiveCocoa/View/MRCNewsViewController.m
@@ -33,6 +33,16 @@
     
     self.attributedLabel = [[DTAttributedLabel alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH - 10 - 40 - 10 - 10, 15)];
     self.attributedLabel.layoutFrameHeightIsConstrainedByBounds = NO;
+    
+    @weakify(self)
+    [self.viewModel.requestRemoteDataCommand.executing subscribeNext:^(NSNumber *executing) {
+        @strongify(self)
+        if (executing.boolValue && self.viewModel.dataSource == nil) {
+            [MBProgressHUD showHUDAddedTo:self.view animated:YES].labelText = MBPROGRESSHUD_LABEL_TEXT;
+        } else {
+            [MBProgressHUD hideHUDForView:self.view animated:YES];
+        }
+    }];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView dequeueReusableCellWithIdentifier:(NSString *)identifier forIndexPath:(NSIndexPath *)indexPath {
--- MVVMReactiveCocoa/ViewModel/ItemViewModel/MRCNewsItemViewModel.m
+++ MVVMReactiveCocoa/ViewModel/ItemViewModel/MRCNewsItemViewModel.m
@@ -7,11 +7,14 @@
 //
 
 #import "MRCNewsItemViewModel.h"
+#import "TTTTimeIntervalFormatter.h"
 
 @interface MRCNewsItemViewModel ()
 
 @property (strong, nonatomic, readwrite) OCTEvent *event;
+@property (strong, nonatomic, readwrite) NSString *imageIdentifier;
 @property (strong, nonatomic, readwrite) NSAttributedString *contentAttributedString;
+@property (copy, nonatomic, readwrite) NSString *occurTime;
 
 @end
 
@@ -21,14 +24,16 @@
     self = [super init];
     if (self) {
         self.event = event;
-        
+
         NSMutableAttributedString *attributedString = nil;
         NSString *string = nil;
         NSString *target = event.repositoryName;
         
         NSDictionary *attributes = @{ NSForegroundColorAttributeName: HexRGB(0x4078c0),
-                                      NSFontAttributeName: [UIFont boldSystemFontOfSize:15] };
+                                      NSFontAttributeName: [UIFont boldSystemFontOfSize:16] };
         
+        self.imageIdentifier = @"Unknown";
+
         if ([event.type isEqualToString:@"CommitCommentEvent"]) {
             OCTCommitCommentEvent *concreteEvent = (OCTCommitCommentEvent *)event;
             
@@ -36,9 +41,11 @@
             string = [NSString stringWithFormat:@"%@ commented on commit %@", concreteEvent.actorLogin, commit];
             
             attributedString = [[NSMutableAttributedString alloc] initWithString:string];
-            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:15] range:[string rangeOfString:string]];
+            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:16] range:[string rangeOfString:string]];
             
             target = commit;
+            
+            self.imageIdentifier = @"CommentDiscussion";
         } else if ([event.type isEqualToString:@"CreateEvent"] || [event.type isEqualToString:@"DeleteEvent"]) {
             OCTRefEvent *concreteEvent = (OCTRefEvent *)event;
             
@@ -52,10 +59,13 @@
             NSString *object = @"";
             if (concreteEvent.refType == OCTRefTypeBranch) {
                 object = @"branch";
+                self.imageIdentifier = @"GitBranch";
             } else if (concreteEvent.refType == OCTRefTypeTag) {
                 object = @"tag";
+                self.imageIdentifier = @"Tag";
             } else if (concreteEvent.refType == OCTRefTypeRepository) {
                 object = @"repository";
+                self.imageIdentifier = @"Repo";
             }
             
             NSString *refName = concreteEvent.refName ? [concreteEvent.refName stringByAppendingString:@" "] : @"";
@@ -64,7 +74,7 @@
             string = [NSString stringWithFormat:@"%@ %@ %@ %@%@%@", concreteEvent.actorLogin, action, object, refName, at, concreteEvent.repositoryName];
             attributedString = [[NSMutableAttributedString alloc] initWithString:string];
             
-            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:15] range:[string rangeOfString:string]];
+            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:16] range:[string rangeOfString:string]];
             [attributedString addAttributes:attributes range:[string rangeOfString:refName]];
         } else if ([event.type isEqualToString:@"ForkEvent"]) {
             OCTForkEvent *concreteEvent = (OCTForkEvent *)event;
@@ -72,8 +82,10 @@
             string = [NSString stringWithFormat:@"%@ forked %@ to %@", concreteEvent.actorLogin, concreteEvent.repositoryName, concreteEvent.forkedRepositoryName];
             attributedString = [[NSMutableAttributedString alloc] initWithString:string];
             
-            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:15] range:[string rangeOfString:string]];
+            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:16] range:[string rangeOfString:string]];
             [attributedString addAttributes:attributes range:[string rangeOfString:concreteEvent.forkedRepositoryName]];
+            
+            self.imageIdentifier = @"GitBranch";
         } else if ([event.type isEqualToString:@"IssueCommentEvent"]) {
             OCTIssueCommentEvent *concreteEvent = (OCTIssueCommentEvent *)event;
             
@@ -81,19 +93,24 @@
             string = [NSString stringWithFormat:@"%@ commented on issue %@", concreteEvent.actorLogin, issue];
             attributedString = [[NSMutableAttributedString alloc] initWithString:string];
             
-            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:15] range:[string rangeOfString:string]];
+            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:16] range:[string rangeOfString:string]];
             
             target = issue;
+            
+            self.imageIdentifier = @"CommentDiscussion";
         } else if ([event.type isEqualToString:@"IssuesEvent"]) {
             OCTIssueEvent *concreteEvent = (OCTIssueEvent *)event;
             
             NSString *action = @"";
             if (concreteEvent.action == OCTIssueActionOpened) {
                 action = @"opened";
+                self.imageIdentifier = @"IssueOpened";
             } else if (concreteEvent.action == OCTIssueActionClosed) {
                 action = @"closed";
+                self.imageIdentifier = @"IssueClosed";
             } else if (concreteEvent.action == OCTIssueActionReopened) {
                 action = @"reopened";
+                self.imageIdentifier = @"IssueReopened";
             } else if (concreteEvent.action == OCTIssueActionSynchronized) {
                 action = @"synchronized";
             }
@@ -102,7 +119,7 @@
             string = [NSString stringWithFormat:@"%@ %@ issue %@", concreteEvent.actorLogin, action, issue];
             attributedString = [[NSMutableAttributedString alloc] initWithString:string];
             
-            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:15] range:[string rangeOfString:string]];
+            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:16] range:[string rangeOfString:string]];
             
             target = issue;
         } else if ([event.type isEqualToString:@"MemberEvent"]) {
@@ -111,7 +128,7 @@
             string = [NSString stringWithFormat:@"%@ added %@ to %@", concreteEvent.actorLogin, concreteEvent.memberLogin, concreteEvent.repositoryName];
             attributedString = [[NSMutableAttributedString alloc] initWithString:string];
             
-            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:15] range:[string rangeOfString:string]];
+            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:16] range:[string rangeOfString:string]];
             [attributedString addAttributes:attributes range:[string rangeOfString:concreteEvent.memberLogin]];
         } else if ([event.type isEqualToString:@"PublicEvent"]) {
             OCTPublicEvent *concreteEvent = (OCTPublicEvent *)event;
@@ -119,7 +136,7 @@
             string = [NSString stringWithFormat:@"%@ open sourced %@", concreteEvent.actorLogin, concreteEvent.repositoryName];
             attributedString = [[NSMutableAttributedString alloc] initWithString:string];
             
-            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:15] range:[string rangeOfString:string]];
+            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:16] range:[string rangeOfString:string]];
         } else if ([event.type isEqualToString:@"PullRequestEvent"]) {
             OCTPullRequestEvent *concreteEvent = (OCTPullRequestEvent *)event;
             
@@ -138,9 +155,11 @@
             string = [NSString stringWithFormat:@"%@ %@ pull request %@", concreteEvent.actorLogin, action, pullRequest];
             attributedString = [[NSMutableAttributedString alloc] initWithString:string];
             
-            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:15] range:[string rangeOfString:string]];
+            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:16] range:[string rangeOfString:string]];
             
             target = pullRequest;
+            
+            self.imageIdentifier = @"GitPullRequest";
         } else if ([event.type isEqualToString:@"PullRequestReviewCommentEvent"]) {
             OCTPullRequestCommentEvent *concreteEvent = (OCTPullRequestCommentEvent *)event;
             
@@ -148,32 +167,42 @@
             string = [NSString stringWithFormat:@"%@ commented on pull request %@", concreteEvent.actorLogin, pullRequest];
             attributedString = [[NSMutableAttributedString alloc] initWithString:string];
             
-            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:15] range:[string rangeOfString:string]];
+            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:16] range:[string rangeOfString:string]];
             
             target = pullRequest;
+            
+            self.imageIdentifier = @"CommentDiscussion";
         } else if ([event.type isEqualToString:@"PushEvent"]) {
             OCTPushEvent *concreteEvent = (OCTPushEvent *)event;
             
             string = [NSString stringWithFormat:@"%@ pushed to %@ at %@", concreteEvent.actorLogin, concreteEvent.branchName, concreteEvent.repositoryName];
             attributedString = [[NSMutableAttributedString alloc] initWithString:string];
             
-            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:15] range:[string rangeOfString:string]];
+            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:16] range:[string rangeOfString:string]];
             [attributedString addAttributes:attributes range:[string rangeOfString:concreteEvent.branchName]];
+            
+            self.imageIdentifier = @"GitCommit";
         } else if ([event.type isEqualToString:@"WatchEvent"]) {
             OCTWatchEvent *concreteEvent = (OCTWatchEvent *)event;
             
             string = [NSString stringWithFormat:@"%@ starred %@", concreteEvent.actorLogin, concreteEvent.repositoryName];
             attributedString = [[NSMutableAttributedString alloc] initWithString:string];
             
-            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:15] range:[string rangeOfString:string]];
+            [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:16] range:[string rangeOfString:string]];
+            
+            self.imageIdentifier = @"Star";
         } else {
-            NSLog(@"type: %@", event.type);
+            NSLog(@"Unknown event type: %@", event.type);
         }
 
         [attributedString addAttributes:attributes range:[string rangeOfString:event.actorLogin]];
         [attributedString addAttributes:attributes range:[string rangeOfString:target]];
         
         self.contentAttributedString = attributedString;
+        
+        TTTTimeIntervalFormatter *timeIntervalFormatter = [TTTTimeIntervalFormatter new];
+        timeIntervalFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];
+        self.occurTime = [timeIntervalFormatter stringForTimeIntervalFromDate:[NSDate date] toDate:event.date].copy;
     }
     return self;
 }
