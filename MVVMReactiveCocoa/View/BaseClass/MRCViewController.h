9ce132ed788b08ff5361ae06eacba3a01c479a11
leichunfeng <307213080@qq.com>
2015-11-20 17:23:32 +0800

    Add some comments.
    Former-commit-id: 1a6d799c9afb9a3e743bd856b189f6051c21ace1

--- MVVMReactiveCocoa/View/BaseClass/MRCViewController.h
+++ MVVMReactiveCocoa/View/BaseClass/MRCViewController.h
@@ -8,17 +8,17 @@
 
 @interface MRCViewController : UIViewController
 
-// Initialization method. This is the preferred way to create a new view.
+/// Initialization method. This is the preferred way to create a new view.
 //
-// viewModel - corresponding view model
+/// viewModel - corresponding view model
 //
-// Returns a new view.
+/// Returns a new view.
 - (instancetype)initWithViewModel:(MRCViewModel *)viewModel;
 
-// The `viewModel` parameter in `-initWithViewModel:` method.
-@property (nonatomic, strong, readonly) MRCViewModel * viewModel;
+/// The `viewModel` parameter in `-initWithViewModel:` method.
+@property (nonatomic, strong, readonly) MRCViewModel *viewModel;
 
-// Binds the corresponding view model to the view.
+/// Binds the corresponding view model to the view.
 - (void)bindViewModel;
 
 @end
--- MVVMReactiveCocoa/ViewModel/BaseClass/MRCViewModel.h
+++ MVVMReactiveCocoa/ViewModel/BaseClass/MRCViewModel.h
@@ -10,27 +10,31 @@
 
 @protocol MRCViewModelServices;
 
+/// The type of the title view.
 typedef NS_ENUM(NSUInteger, MRCTitleViewType) {
+    /// System title view
     MRCTitleViewTypeDefault,
+    /// Double title view
     MRCTitleViewTypeDoubleTitle,
+    /// Loading title view
     MRCTitleViewTypeLoadingTitle
 };
 
 /// An abstract class representing a view model.
 @interface MRCViewModel : NSObject
 
-// Initialization method. This is the preferred way to create a new viewModel.
+/// Initialization method. This is the preferred way to create a new view model.
 //
-// services - The service bus of Model layer.
-// params   - The parameters to be passed to view model.
+/// services - The service bus of the `Model` layer.
+/// params   - The parameters to be passed to view model.
 //
-// Returns a new view model.
+/// Returns a new view model.
 - (instancetype)initWithServices:(id<MRCViewModelServices>)services params:(id)params;
 
-// The `services` parameter in `-initWithServices:params:` method.
+/// The `services` parameter in `-initWithServices:params:` method.
 @property (nonatomic, strong, readonly) id<MRCViewModelServices> services;
 
-// The `params` parameter in `-initWithServices:params:` method.
+/// The `params` parameter in `-initWithServices:params:` method.
 @property (nonatomic, strong, readonly) id params;
 
 @property (nonatomic, assign) MRCTitleViewType titleViewType;
@@ -38,10 +42,10 @@ typedef NS_ENUM(NSUInteger, MRCTitleViewType) {
 @property (nonatomic, copy) NSString *title;
 @property (nonatomic, copy) NSString *subtitle;
 
-// The callback block.
+/// The callback block.
 @property (nonatomic, copy) VoidBlock_id callback;
 
-// A RACSubject object, which representing all errors occurred in view model.
+/// A RACSubject object, which representing all errors occurred in view model.
 @property (nonatomic, strong, readonly) RACSubject *errors;
 
 @property (nonatomic, assign) BOOL shouldFetchLocalDataOnViewModelInitialize;
@@ -49,10 +53,10 @@ typedef NS_ENUM(NSUInteger, MRCTitleViewType) {
 
 @property (nonatomic, strong, readonly) RACSubject *willDisappearSignal;
 
-// An additional method, in which you can initialize data, RACCommand etc.
+/// An additional method, in which you can initialize data, RACCommand etc.
 //
-// This method will be execute after the execution of `-initWithServices:params:` method. But
-// the premise is that you need to inherit `MRCViewModel`.
+/// This method will be execute after the execution of `-initWithServices:params:` method. But
+/// the premise is that you need to inherit `MRCViewModel`.
 - (void)initialize;
 
 @end
