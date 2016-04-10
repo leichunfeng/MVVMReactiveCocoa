df5a7a73ba3b616113782b03bcd32fe42bbafd16
leichunfeng <307213080@qq.com>
2016-04-10 14:41:19 +0800

    Delete useless code

--- MVVMReactiveCocoa/View/BaseClass/MRCTabBarController.m
+++ MVVMReactiveCocoa/View/BaseClass/MRCTabBarController.m
@@ -27,49 +27,15 @@
 }
 
 - (BOOL)shouldAutorotate {
-    return self.tabBarController.shouldAutorotate;
+    return self.tabBarController.selectedViewController.shouldAutorotate;
 }
 
 - (UIInterfaceOrientationMask)supportedInterfaceOrientations {
-    return self.tabBarController.supportedInterfaceOrientations;
+    return self.tabBarController.selectedViewController.supportedInterfaceOrientations;
 }
 
 - (UIStatusBarStyle)preferredStatusBarStyle {
-    return self.tabBarController.preferredStatusBarStyle;
-}
-
-@end
-
-@interface UITabBarController (MRCStatusBarAddtions)
-
-@end
-
-@implementation UITabBarController (MRCStatusBarAddtions)
-
-+ (void)load {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        Class class = [self class];
-
-        SEL originalSelector = @selector(preferredStatusBarStyle);
-        SEL swizzledSelector = @selector(mrc_preferredStatusBarStyle);
-
-        Method originalMethod = class_getInstanceMethod(class, originalSelector);
-        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-
-        BOOL success = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
-        if (success) {
-            class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
-    });
-}
-
-#pragma mark - Method Swizzling
-
-- (UIStatusBarStyle)mrc_preferredStatusBarStyle {
-    return self.selectedViewController.preferredStatusBarStyle;
+    return self.tabBarController.selectedViewController.preferredStatusBarStyle;
 }
 
 @end
